a0a5168c48f2534f10c9aeac336b95b251c3dbfa
Matt Hamil <j.matthew.hamil@gmail.com>
2018-12-08 19:53:33 -0600

    chore: added clean-webpack-plugin to clean dist on builds

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
@@ -6,5 +7,8 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     filename: 'reducer-composer.js',
     library: 'reducer-composer',
-  }
+  },
+  plugins: [
+    new CleanWebpackPlugin(['dist']),
+  ]
 };
